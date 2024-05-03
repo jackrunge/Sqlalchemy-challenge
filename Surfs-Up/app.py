5f5376b26ee0305e4775980a6d7b6ba5b5730814
Jack Runge <jrunge96@gmail.com>
2024-05-02 19:48:02 -0600

    Fixed another typo, and some methods called incorrectly

--- Surfs-Up/app.py
+++ Surfs-Up/app.py
@@ -41,7 +41,7 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api.v1.0/-startdate-<br/>"
+        f"/api/v1.0/-startdate-<br/>"
         f"/api/v1.0/-startdate-/-enddate-<br/>"
     )
 
